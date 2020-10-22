7b8fc25eda26c30465a7a82b111ffd22c28b0d49
Piter815 <pomaranski.piotr@gmail.com>
2020-10-22 19:24:26 +0200

    Director & Genre model added

--- django_movies/core/admin.py
+++ django_movies/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import Movies, Genre, Countries
+from core.models import Movies, Genre, Countries, Director
 
 admin.site.register(Movies)
 admin.site.register(Genre)
 admin.site.register(Countries)
+admin.site.register(Director)
--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(max_length=20, unique=True)
+    age_limit = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
@@ -16,6 +17,16 @@ class Countries(models.Model):
         return self.name
 
 
+class Director(models.Model):
+    name = models.CharField(max_length=20)
+    surname = models.CharField(max_length=20)
+
+
+
+    def __str__(self):
+        return f'{self.name}, {self.surname}'
+
+
 class Movies(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(null=True, validators=[MaxValueValidator(10), MinValueValidator(1)])
@@ -24,6 +35,7 @@ class Movies(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     genre = models.ForeignKey(Genre, null=True, on_delete=models.DO_NOTHING)
     country = models.ForeignKey(Countries, null=True, on_delete=models.DO_NOTHING)
+    director = models.ForeignKey(Director, null=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f'{self.title} from {self.released}'
