09c218a0a040977ff51faa07cf7d014810548912
Piter <pomaranski.piotr@gmail.com>
2020-10-22 13:35:40 +0200

    countries and genre models added, movie entries added

--- django_movies/core/admin.py
+++ django_movies/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from core.models import Movies
+from core.models import Movies, Genre, Countries
 
 admin.site.register(Movies)
-
+admin.site.register(Genre)
+admin.site.register(Countries)
--- django_movies/core/models.py
+++ django_movies/core/models.py
@@ -1,12 +1,29 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Countries(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Movies(models.Model):
     title = models.CharField(max_length=100)
     rating = models.IntegerField(null=True, validators=[MaxValueValidator(10), MinValueValidator(1)])
     released = models.DateField()
     description = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    genre = models.ForeignKey(Genre, null=True, on_delete=models.DO_NOTHING)
+    country = models.ForeignKey(Countries, null=True, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return f'{self.title} from {self.released}'
