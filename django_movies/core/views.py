4fa44c18ad4888b8ce1b8e26eb5abce41d5678e1
Piter <pomaranski.piotr@gmail.com>
2020-10-27 14:45:55 +0100

    add age limiter movies view shows only "kids"&"teens"

--- django_movies/core/views.py
+++ django_movies/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
 from core.models import Movies
 
 
@@ -10,9 +11,21 @@ def hello(request):
         context={'adjectives':['beautiful','cruel','wonderful']},
     )
 
-def movies(request):
-    return render(
-        request,
-        template_name='movies.html',
-        context={'movies': Movies.objects.all()},
-    )
+
+class MovieView(ListView):
+    template_name = 'movies.html'
+    model = Movies
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['movie_list'] = Movies.objects.filter(genre__age_limit__lte=1)
+        return context
+
+# def movies(request):
+#     return render(
+#         request,
+#         template_name='movies.html',
+#         context={'movies': Movies.objects.all()},
+#     )
--- django_movies/django_movies/urls.py
+++ django_movies/django_movies/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import hello, movies
+from core.views import hello, MovieView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello),
-    path("", movies, name='index')
+    path("", MovieView.as_view(), name='index')
 
 ]
